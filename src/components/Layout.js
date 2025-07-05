6048c28306ea9e268d166fef5ea1c6c48c72c315
Joseph Telaak <jttelaak@hotmail.com>
2025-07-05 16:29:11 -0400

    footer fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,11 +5,13 @@ import Footer from "./Footer";
 const Layout = ({ children }) => (
   <div className="App">
     <Navbar />
-    <header className="hero" style={{ paddingTop: '80px', paddingBottom: '40px' }}>
-      <h1 style={{ fontSize: '6rem', fontWeight: 'bold' }}>Parkeze</h1>
-      <p style={{ fontSize: '1.5rem' }}>Finding parking so you don't have to</p>
-    </header>
-    {children}
+    <div className="main-content">
+      <header className="hero" style={{ paddingTop: '80px', paddingBottom: '40px' }}>
+        <h1 style={{ fontSize: '6rem', fontWeight: 'bold' }}>Parkeze</h1>
+        <p style={{ fontSize: '1.5rem' }}>Finding parking so you don't have to</p>
+      </header>
+      {children}
+    </div>
     <Footer />
   </div>
 );
