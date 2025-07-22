092267be8f5d3acd648b419a6d0f93e20fb22d9f
Joseph Telaak <47280152+The1TrueJoe@users.noreply.github.com>
2025-07-21 20:41:01 -0400

    refactor parkezesolutions to parkeze

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="footer" id="contact">
     <p>&copy; 2025 Parkeze LLC. All rights reserved.</p>
-    <p>Contact us: <a href="mailto:contact@parkezesolutions.com">contact@parkezesolutions.com</a></p>
+    <p>Contact us: <a href="mailto:contact@parkeze.com">contact@parkeze.com</a></p>
     <p><a href="https://www.linkedin.com/company/parkeze/" target="_blank" rel="noopener noreferrer">Connect with us on LinkedIn</a></p>
   </footer>
 );
